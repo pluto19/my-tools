4cd4f7bc882ed7bb58b1b153d7d667ee6bda9045
pluto19 <1109319321@qq.com>
2025-02-11 15:29:46 +0800

    添加Google AdSense脚本支持及相关初始化代码

---
+++ docs/assets/js/adsense.js
@@ -0,0 +1,8 @@
+// Google AdSense 初始化代码
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9024574583281237';
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+})();
